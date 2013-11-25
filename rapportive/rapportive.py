19df89bb2d9d4c789d9e2bfb61efeaa24fc42f89
Hugh Brown <hbrown@wgen.net>
2013-11-24 18:24:58 -0700

    Fix pylint and pep8 errors

--- rapportive/rapportive.py
+++ rapportive/rapportive.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 """
-usage: rapportive.py [-h] [--output OUTPUT] [--email EMAIL] [--verbose]
+usage: rapportive.py [-h]
+                     [--output OUTPUT]
+                     [--email EMAIL]
+                     [--verbose]
 
 Check list of emails using Rapportive API
 
@@ -25,13 +28,20 @@ import requests
 from docopt import docopt
 
 
-logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                    datefmt='%m-%d %H:%M')
-logger = logging.getLogger('rapportive')
+logging.basicConfig(
+    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+    datefmt='%m-%d %H:%M')
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+STATUS_URL = 'https://rapportive.com/login_status?user_email={0}'
+URL = 'https://profiles.rapportive.com/contacts/email/{0}'
 
 
 class Profile(object):
+    """
+    Profile object for collecting profile information before printing
+    """
     def __init__(self, person):
         if person:
             self.name = person.get('name')
@@ -52,22 +62,26 @@ class Profile(object):
             {2}
         """).format(
             self.name,
-            "\n".join("{0} {1}".format(title, company) for title, company in self.jobinfo),
-            "\n".join("\t{0} {1}".format(site_name, url) for site_name, url in self.memberships)
+            "\n".join(
+                "{0} {1}".format(title, company)
+                for title, company in self.jobinfo),
+            "\n".join(
+                "\t{0} {1}".format(site_name, url)
+                for site_name, url in self.memberships)
         )
 
 
 def request(email):
     '''
-    rapportive_request(email): Sends a query to the undocumented Rapportive API using a random gmail address
+    rapportive_request(email): Sends a query to the undocumented Rapportive API
                                Returns the response as a dict
     '''
-    status_url = 'https://rapportive.com/login_status?user_email={0}'.format(email)
+    status_url = STATUS_URL.format(email)
     response = requests.get(status_url).json()
     session_token = response.get('session_token')
     if response['status'] == 200 and session_token:
         logger.debug('Session token: {0}'.format(session_token))
-        url = 'https://profiles.rapportive.com/contacts/email/{0}'.format(email)
+        url = URL.format(email)
         headers = {'X-Session-Token': session_token}
         return requests.get(url, headers=headers).json()
     return {}
@@ -75,12 +89,16 @@ def request(email):
 
 def parse_summary(profile):
     '''
-    rapportive_summary(profile): Returns a summary of the Rapportive results for an email address
+    rapportive_summary(profile): Returns a Profile object for an email address
     '''
     return Profile(profile.get('contact'))
 
 
 def ___process_email(email, output_file=None):
+    """
+    Find email address in rapportive and print profile info
+    if found
+    """
     profile = request(email)
     success = profile.get('success')
     if success != 'nothing_useful':
@@ -95,9 +113,8 @@ def ___process_email(email, output_file=None):
 
 def main():
     '''
-    main(): Expect a list of email addresses via stdin and check them with the Rapportive API
-            If the --jigsaw flag is set, format the output from the jigsaw.rb tool and check each
-            address.
+    main(): Expect a list of email addresses via stdin
+            and check them with the Rapportive API
     '''
     options = docopt(__doc__, version="0.1.0")
     if options["--verbose"]:
